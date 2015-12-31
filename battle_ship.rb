3e9744c4d89b10540ed7ed7a0a876959b0de51cc
chiprockstar <chiprockstar@users.noreply.github.com>
2015-12-30 22:47:07 -0500

    Converted to OO version

--- battle_ship.rb
+++ battle_ship.rb
@@ -1,11 +1,12 @@
 
-class Array2
-  #-- this class allows using arrays like 2 dimensional arrays - a[row, col] to set and get.
+class Array2Dimensions
+
   def initialize
     @store = [[]]
+
   end
 
-  def [] (a,b)
+  def [] (a,b)              #-- multideminsional arrays
     if @store[a] == nil ||
       @store[a][b] == nil
       return nil
@@ -21,113 +22,131 @@ class Array2
 
 end
 
-#-- initialize arrays
-a = Array2.new
-b = Array2.new
-computer = Array2.new
-computer_selects = []
-player_selects = []
 
-#-- initialize data structure
-0.upto(4).each do | x |
-  0.upto(4).each do | y |
-    a[x, y] = ' '
-    b[x, y] = ' '
-    computer[x, y] = ' '
+class Battleship
+
+  def initialize
+    #-- initialize arrays
+    @a = Array2Dimensions.new
+    @b = Array2Dimensions.new
+    @computer = Array2Dimensions.new
+    @computer_selects = Array.new
+    @player_selects = Array.new
   end
-end
 
-#-- place ships on both boards randomly
-['a', 'b'].each do | letter |
+  def init_data_struct
+  #-- initialize board locations to 1 space
+    0.upto(4).each do | x |
+      0.upto(4).each do | y |
+        @a[x, y] = ' '
+        @b[x, y] = ' '
+        @computer[x, y] = ' '
+      end
+    end
+  end
+
+
+  def place_cruisers
+    ['@a', '@b'].each do | letter |
     #-- cruiser positions
-  eval("#{letter}")[rand(5), rand(5)] = 'c'
+      eval("#{letter}")[rand(5), rand(5)] = 'c'
+    end
+  end
 
+
+  def place_destroyers
+    ['@a', '@b'].each do | letter |
   #-- destroyer positions
-  destroyer_placed = false
-  while destroyer_placed == false
-    if rand(2) == 0 #-- row or column
-      row = rand(5)
-      col = rand(4)
-      col.upto(col+1).each do | y |
-        if eval("#{letter}")[row, y] != ' '
-          destroyer_placed = false
-          break
-        else
-          destroyer_placed = true
+      destroyer_placed = false
+      while destroyer_placed == false
+        if rand(2) == 0 #-- row or column
+          row = rand(5)
+          col = rand(4)
+          col.upto(col+1).each do | y |
+            if eval("#{letter}")[row, y] != ' '
+              destroyer_placed = false
+              break
+            else
+              destroyer_placed = true
+            end
+          end
+          if destroyer_placed
+            col.upto(col+1).each do | y |
+              eval("#{letter}")[row, y] = 'd'
+            end
+          end
+        else #-- column
+          row = rand(4)
+          col = rand(5)
+          row.upto(row+1).each do | x |
+            if eval("#{letter}")[x, col] != ' '
+              destroyer_placed = false
+              break
+            else
+              destroyer_placed = true
+            end
+          end
+          if destroyer_placed
+            row.upto(row+1).each do | x |
+              eval("#{letter}")[x, col] = 'd'
+            end
+          end
         end
       end
-      if destroyer_placed
-        col.upto(col+1).each do | y |
-          eval("#{letter}")[row, y] = 'd'
-        end
-      end
-    else #-- column
-      row = rand(4)
-      col = rand(5)
-      row.upto(row+1).each do | x |
-        if eval("#{letter}")[x, col] != ' '
-          destroyer_placed = false
-          break
-        else
-          destroyer_placed = true
-        end
-      end
-      if destroyer_placed
-        row.upto(row+1).each do | x |
-          eval("#{letter}")[x, col] = 'd'
+    end
+  end
+
+  def place_battleships
+    ['@a', '@b'].each do | letter |
+        #-- battleship positions
+      battleship_placed = false
+      while battleship_placed == false
+        if rand(2) == 0 #-- row or column
+          row = rand(5)
+          col = rand(3)
+          col.upto(col+2).each do | y |
+            if eval("#{letter}")[row, y] != ' '
+              battleship_placed = false
+              break
+            else
+              battleship_placed = true
+            end
+          end
+          if battleship_placed
+            col.upto(col+2).each do | y |
+              eval("#{letter}")[row, y] = 'b'
+            end
+          end
+        else #-- column
+          row = rand(3)
+          col = rand(5)
+          row.upto(row+2).each do | x |
+            if eval("#{letter}")[x, col] != ' '
+              battleship_placed = false
+              break
+            else
+              battleship_placed = true
+            end
+          end
+          if battleship_placed
+            row.upto(row+2).each do | x |
+              eval("#{letter}")[x, col] = 'b'
+            end
+          end
         end
       end
     end
   end
 
-#-- battleship positions
-  battleship_placed = false
-  while battleship_placed == false
-    if rand(2) == 0 #-- row or column
-      row = rand(5)
-      col = rand(3)
-      col.upto(col+2).each do | y |
-        if eval("#{letter}")[row, y] != ' '
-          battleship_placed = false
-          break
-        else
-          battleship_placed = true
-        end
-      end
-      if battleship_placed
-        col.upto(col+2).each do | y |
-          eval("#{letter}")[row, y] = 'b'
-        end
-      end
-
-    else #-- column
-      row = rand(3)
-      col = rand(5)
-      row.upto(row+2).each do | x |
-        if eval("#{letter}")[x, col] != ' '
-          battleship_placed = false
-          break
-        else
-          battleship_placed = true
-        end
-      end
-      if battleship_placed
-        row.upto(row+2).each do | x |
-          eval("#{letter}")[x, col] = 'b'
-        end
-      end
-    end
-  end
-end
 
 
-coord = ''
-while coord != 'q'
+def draw_boards
+
   system "clear"
 
-  ['a', 'b'].each do  | letter |
+  ['@a', '@b'].each do  | letter |
     puts ""
-    if letter == 'a'
+    if letter == '@a'
       puts "Willy's Board"
     else
       puts "Computer's Board"
@@ -145,10 +164,10 @@ while coord != 'q'
     puts "5 | #{eval(letter)[4,0]} | #{eval(letter)[4,1]} | #{eval(letter)[4,2]} | #{eval(letter)[4,3]} | #{eval(letter)[4,4]} |"
     puts "  +---+---+---+---+---+"
     puts ""
-    turn = 'computer_selects' if letter == 'a'
-    turn = 'player_selects' if letter == 'b'
+    turn = '@computer_selects' if letter == '@a'
+    turn = '@player_selects' if letter == '@b'
 
-    #-- set ship statuses
+    # #-- set ship statuses
     destroyer_status =  'Alive'
     cruiser_status =    'Alive'
     battleship_status = 'Alive'
@@ -159,45 +178,67 @@ while coord != 'q'
     puts "Destroyer: #{destroyer_status}  Cruiser: #{cruiser_status}  Battleship: #{battleship_status}"
 
   end
+end
 
-
-  #-- evaluate game
-  if player_selects.grep('b').size == 3 && player_selects.grep('c').size == 1 && player_selects.grep('d').size == 2
-    puts "Game Over - Willy Wins!"
-    exit
-  elsif computer_selects.grep('b').size == 3 && computer_selects.grep('c').size == 1 && computer_selects.grep('d').size == 2
-    puts "Game Over - Computer Wins!"
-    exit
-  end
-
-
-  #-- player takes turn
-  puts "Select a square such as (3,5) to open fire. Enter q to quit."
-  coord = gets.chomp
-  split_value = coord.split(",").map { |x| x.to_i } if coord != 'q'
-  player_selects << "#{b[split_value.last - 1, split_value.first - 1]}"
-
-  if b[split_value.last - 1, split_value.first - 1] == ' ' && coord != 'q'
-    b[split_value.last - 1, split_value.first - 1] = '/'
-  else
-    b[split_value.last - 1, split_value.first - 1] = 'x'
-  end
-
-
-  #-- computer takes turn
-  process = true
-  while process
-    row = rand(5)
-    col = rand(5)
-    if !computer[row, col].include?("x") && !computer[row, col].include?("/")
-      computer_selects << "#{a[row, col]}"
-      if a[row, col] == ' '
-        a[row, col]  = '/'
-      elsif a[row, col].scan(/\w/)
-        a[row, col] = 'x'
-      end
-      computer[row, col] = "#{a[row, col]}"
-      process = false
+  def evaluate_game
+    #-- evaluate game
+    if @player_selects.grep('b').size == 3 && @player_selects.grep('c').size == 1 && @player_selects.grep('d').size == 2
+      puts "Game Over - Willy Wins!"
+      exit
+    elsif @computer_selects.grep('b').size == 3 && @computer_selects.grep('c').size == 1 && @computer_selects.grep('d').size == 2
+      puts "Game Over - Computer Wins!"
+      exit
     end
   end
+
+  def player_turn
+    #-- player takes turn
+    puts "Select a square such as (3,5) to open fire. Enter q to quit."
+    coord = gets.chomp
+    exit if coord == 'q'
+    split_value = coord.split(",").map { |x| x.to_i }
+    @player_selects << "#{@b[split_value.last - 1, split_value.first - 1]}"
+
+    if @b[split_value.last - 1, split_value.first - 1] == ' '
+      @b[split_value.last - 1, split_value.first - 1] = '/'
+    else
+      @b[split_value.last - 1, split_value.first - 1] = 'x'
+    end
+  end
+
+  def computer_turn
+    #-- computer takes turn
+    process = true
+    while process
+      row = rand(5)
+      col = rand(5)
+      if !@computer[row, col].include?("x") && !@computer[row, col].include?("/")
+        @computer_selects << "#{@a[row, col]}"
+        if @a[row, col] == ' '
+          @a[row, col]  = '/'
+        elsif @a[row, col].scan(/\w/)
+          @a[row, col] = 'x'
+        end
+        @computer[row, col] = "#{@a[row, col]}"
+        process = false
+      end
+    end
+  end
+end
+
+
+
+c = Battleship.new
+c.init_data_struct
+c.place_cruisers
+c.place_destroyers
+c.place_battleships
+c.draw_boards
+while 1 == 1
+  c.player_turn
+  c.draw_boards
+  c.evaluate_game
+  c.computer_turn
+  c.draw_boards
+  c.evaluate_game
 end
